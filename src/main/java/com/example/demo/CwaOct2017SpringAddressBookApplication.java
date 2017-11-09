2fad48c6337c6bacf253111d078a8f3aac1e5783
Joyce Yang <jcyang@hmc.edu>
2017-11-09 11:30:00 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/CwaOct2017SpringAddressBookApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwaOct2017SpringAddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwaOct2017SpringAddressBookApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,66 @@
+package com.example.demo;
+
+
+import org.hibernate.validator.constraints.Email;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Person {
+@Id
+@GeneratedValue(strategy= GenerationType.AUTO)
+private long id;
+
+@NotNull
+@Size(min=2)
+private String name;
+
+@NotNull
+@Email
+private String email;
+
+@Min(3)
+private int phone;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
+
+
+
