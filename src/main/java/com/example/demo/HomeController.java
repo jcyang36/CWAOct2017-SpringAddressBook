ed53bf792d3850481a6fd30cd6ed64a5131e2876
Joyce Yang <jcyang@hmc.edu>
2017-11-11 23:07:14 -0500

    Background updates

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -16,10 +16,20 @@ public class HomeController {
     @Autowired
     PersonRepository personRepository;
 
-    @RequestMapping("/")
+    @RequestMapping("/list")
     public String listPersons(Model model){
         model.addAttribute("people", personRepository.findAll());
     return "list";}
+
+    @RequestMapping("/")
+    public String goHomePage(Model model){
+        model.addAttribute("people", personRepository.findAll());
+        return "main";}
+
+    @RequestMapping("/main")
+    public String goMainPage(Model model){
+        model.addAttribute("people", personRepository.findAll());
+        return "main";}
 
     @GetMapping("/add")
     public String addPerson(Model model){
@@ -32,7 +42,7 @@ public class HomeController {
         return "personform";
     }
     personRepository.save(person);
-    return "redirect:/";
+    return "redirect:/list";
     }
     @RequestMapping("/detail/{id}")
     public String showPerson(@PathVariable("id") long id, Model model){model.addAttribute("person", personRepository.findOne(id));
